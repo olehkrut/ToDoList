400f24e0fa94d2f6acc892eb895ce4d2c0ec0ccb
olehkrut <olegkrutiy@gmail.com>
2015-12-04 23:47:38 +0200

    installed Owin packages

---
+++ ToDoList/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(ToDoList.App_Start.Startup)]
+namespace ToDoList.App_Start
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			HttpConfiguration config = new HttpConfiguration();
+			WebApiConfig.Register(config);
+			app.UseWebApi(config);
+		}
+	}
+}
