20a2a6957e456124f2f23cec80558ff2271f2c94
olehkrut <olegkrutiy@gmail.com>
2015-12-05 11:57:48 +0200

    Added DataAccess.Configuration and DataAccess.Entities projects

---
+++ DataAccess.Entities/ToDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+	public class ToDoItem
+	{
+		public int ToDoItemId { get; set; }
+		public string ToDoContent { get; set; }
+		public byte Priority { get; set; }
+		public DateTime? DueDate { get; set; }
+
+		public virtual ApplicationUser Owner { get; set; }
+		public virtual string OwnerId { get; set; }
+	}
+}
---
+++ DataAccessLayer/ToDoListDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+	public class ToDoListDbContext: IdentityDbContext<ApplicationUser>
+	{
+		public ToDoListDbContext(): 
+			base("ToDoListDbContext")
+		{
+		}
+
+		public DbSet<ApplicationUser> Users { get; set; }
+		public DbSet<ToDoItem> ToDoItems { get; set; }
+
+    }
+}
