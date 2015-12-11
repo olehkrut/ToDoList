dc5bf6c2c789f475d4853c9a7b4b4b87927ed04c
olehkrut <olegkrutiy@gmail.com>
2015-12-11 23:54:21 +0200

    login page and login style sheet was added

--- DataAccess.Entities/ApplicationUser.cs
+++ DataAccess.Entities/ApplicationUser.cs
@@ -5,6 +5,13 @@ namespace DataAccess.Entities
 {
     public class ApplicationUser : IdentityUser
     {
+		public ApplicationUser()
+		{
+			Items = new List<ToDoItem>();
+		}
+
+		public string Login { get; set; }
+		public string Password { get; set; }
 		public virtual ICollection<ToDoItem> Items { get; set; }
     }
 }
