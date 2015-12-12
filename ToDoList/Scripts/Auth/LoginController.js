02a66b2b663a846d7bd54655d710033e07784213
olehkrut <olegkrutiy@gmail.com>
2015-12-12 14:42:55 +0200

    authService.js was completed

--- ToDoList/Scripts/Auth/LoginController.js
+++ ToDoList/Scripts/Auth/LoginController.js
@@ -1,6 +1,15 @@
 ﻿app.controller("LoginController", ["$scope", "$location", "$modal", "AuthService",
 	function ($scope, $location, $modal, AuthService) {
 		$scope.login = function () {
-			if($scope.)
+			if ($scope.loginForm.$valid) {
+				AuthService.login({
+					userName: $scope.userName,
+					password: $scope.password
+				}).then(function (ok) {
+
+				}, function (error) {
+
+				});
+			}
 		}
 }]);
