3418c21b7ef315e8d412ac6d6f75f525bdf9a046
pollyub <pollyub@gmail.com>
2015-12-12 21:36:36 +0200

    edit+add

--- ToDoList/Controllers/ToDoItemController.cs
+++ ToDoList/Controllers/ToDoItemController.cs
@@ -5,11 +5,17 @@ using System.Linq;
 using System.Web.Http;
 using DataAccess.Entities;
 using DataAccessLayer;
+using System.Net.Http;
+using System.Web;
+using System.Net;
+using System.Data.Entity;
 
 namespace ToDoList.Controllers
 {
 	public class ToDoItemController : ApiController
 	{
+        private readonly ToDoListDbContext db = new ToDoListDbContext();
+
 		public IEnumerable<ToDoItem> Get()
 		{
 			using (ToDoListDbContext context = new ToDoListDbContext())
@@ -18,6 +24,47 @@ namespace ToDoList.Controllers
 				return context.ToDoItems.ToList();
 			}
 		}
+
+
+        public HttpResponseMessage PutTodoItem(int id, ToDoItem todoitem)
+        {
+            if (ModelState.IsValid && id == todoitem.ToDoItemId)
+            {
+                db.Entry(todoitem).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        public HttpResponseMessage PostTodoItem(ToDoItem todoitem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ToDoItems.Add(todoitem);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, todoitem);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = todoitem.ToDoItemId }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
 
 
 		// POST api/<controller>
--- ToDoList/Scripts/app.js
+++ ToDoList/Scripts/app.js
@@ -5,10 +5,8 @@
 			controller: 'ToDoItemListController',
 			templateUrl: 'Scripts/ToDoItemList/toDoItemList.html'
 		})
-			.when('/new', {
-				controller: 'CreateNewItemController',
-				templateUrl: 'Scripts/NewItem/detail.html'
-			})
+			.when('/new', { controller: CreateCtrl, templateUrl: 'detail.html' })
+            .when('/edit/:itemId', { controller: EditCtrl, templateUrl: 'detail.html' })
 			.when('/', {
 				controller: "LoginController",
 				templateUrl: "Scripts/Auth/LoginPage.html"
@@ -17,6 +15,27 @@
 				redirectTo: '/'
 			});
 	});
+
+var CreateCtrl = function ($scope, $location, Todo) {
+    $scope.btnName = "Add";
+
+    $scope.save = function () {
+        Todo.save($scope.item, function () {
+            $location.path('/');
+        });
+    };
+};
+
+var EditCtrl = function ($scope, $routeParams, $location, Todo) {
+    $scope.item = Todo.get({ id: $routeParams.itemId });
+    $scope.btnName = "Edit";
+
+    $scope.save = function () {
+        Todo.update({ id: $scope.item.TodoItemId }, $scope.item, function () {
+            $location.path('/');
+        });
+    };
+};
 
 app.controller("IndexController", function ($scope, $location, localStorageService) {
 	$scope.isAuthorize = function () {
